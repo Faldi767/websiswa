7b69afca851f6ead891cfabc3c8650247d890fa4
Faldi767 <Faldi767@gmail.com>
2020-01-26 13:29:31 +0700

    View Tabel Siswa

--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -1,17 +1,44 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
         <title>Web Siswa</title>
     </head>
     <body>
-
-    <h1>Data Siswa</h1>
-
-    <ul>
-        @foreach($siswa as $s)
-            <li>{{ "Nama : ". $s->nama . ' | Alamat : ' . $s->alamat }}</li>
-        @endforeach
-    </ul>
-
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    CRUD Data Siswa
+                </div>
+                <div class="card-body">
+                    <a href="/siswa/tambah" class="btn btn-primary">Input Siswa Baru</a>
+                    <br/>
+                    <br/>
+                    <table class="table table-bordered table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>Alamat</th>
+                                <th>OPSI</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($siswa as $s)
+                            <tr>
+                                <td>{{ $s->nama }}</td>
+                                <td>{{ $s->alamat }}</td>
+                                <td>
+                                    <a href="/pegawai/edit/{{ $s->id }}" class="btn btn-warning">Edit</a>
+                                    <a href="/pegawai/hapus/{{ $s->id }}" class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
